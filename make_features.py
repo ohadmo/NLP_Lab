0268d63defde31cc45aac516153d59d73eb699dc
Ohad Mosafi <ohadmosafi@gmail.com>
2017-06-22 19:36:25 +0300

    Adding POS-nargs implementation
    Updating function words txt file
    Command line arguments passing

--- make_features.py
+++ make_features.py
@@ -10,15 +10,23 @@ import numpy as np
 import sys
 import datetime
 import time
-from multiprocessing import Process, Queue
-sys.path.append('D:\ohadm\Downloads\libsvm-3.22\python')
-import svmutil
+from multiprocessing import Process, Queue, current_process
+#sys.path.append('D:\ohadm\Downloads\libsvm-3.22\python')
+#import svmutil
+import operator
+from enum import Enum
+import argparse
 
 CHUNK_SIZE = 2000
 
+class SearchWordsOrPOS(Enum):
+    FIND_WORDS = 0
+    FIND_POS = 1
+
+
 def readWordsFromFile(path):
     wordsSet = set()
-    with open(path,'r',encoding='utf-8') as file:
+    with open(path,'r', encoding='utf-8') as file:
         for line in file:
             ln = line.strip().lower().split(',')
             for word in ln:
@@ -38,44 +46,60 @@ def wordsInCorpus(path, words_set):
     return valid_words_in_corpus
 
 def POSinCorpus(path, ngrams):
-    valid_POS = set()
+    all_POS = dict()
     with open(path,'r') as file:
         for line in file:
-            pos = [x.strip().split('_')[1] for x in line.split(' ')]
-            if len(pos) < ngrams:
-                continue
-            for i in range(len(pos) - ngrams + 1):
-                print(pos[i] + "_" + pos[i+1] + "_" + pos[i+2])
+            try:
+                pos_line = [x.strip().split('_')[1] for x in line.split(' ')]
+                if len(pos_line) < ngrams:
+                    continue
+                for i in range(len(pos_line) - ngrams + 1):
+                    pos = ""
+                    for k in range(ngrams):
+                        pos += pos_line[i+k] + "_"
+                    pos = pos[:-1]
+                    if pos not in all_POS:
+                        all_POS[pos] = 1
+                    else:
+                        all_POS[pos] += 1
+            except:
+                print("Exception POS line is:")
+                print(line)
+    valid_POS = sorted(all_POS.items(), key=operator.itemgetter(1), reverse=True) # Decreasing Order
+    temp = [x[0] for x in valid_POS]
+    temp = temp[:400]
+    valid_POS = set(temp)
+    return valid_POS
 
-def ChunkToWordsCounters(chunk, cNouns):
-    tokens = [x.strip().lower().split('_')[0] for x in chunk]
+def ChunkToWordsCounters(chunk, cNouns, idx):
+    tokens = [x.strip().lower().split('_')[idx.value] for x in chunk]
     pronouns_count = {x: 0 for x in cNouns}
     for token in tokens:
         if token in cNouns:
             pronouns_count[token] += 1
-    return [x / len(chunk) for x in pronouns_count.values()]
+    return [x for x in pronouns_count.values()]
 
-def divide_to_chnuks(language_file, label_file, lang, corpusNouns):
+def divide_to_chnuks(language_file, label_file, lang, corpusNouns, search_enum):
     original_chunks = []
     original_chunk = []
     translated_chunks = []
     translated_chunk = []
-    with open(language_file,'r') as dfile, open(label_file,'r') as lfile:
+    with open(language_file, 'r') as dfile, open(label_file, 'r', encoding='utf-8') as lfile:
         for line, label in zip(dfile, lfile):
             tokens = line.split(' ')
             tokens = [x.strip() for x in tokens]
             if label.strip() == lang:
                 original_chunk += tokens
                 if len(original_chunk) > CHUNK_SIZE:
-                    original_chunks.append(ChunkToWordsCounters(original_chunk, corpusNouns))
+                    original_chunks.append(ChunkToWordsCounters(original_chunk, corpusNouns, search_enum))
                     original_chunk = []
             else:
                 translated_chunk += tokens
                 if len(translated_chunk) > CHUNK_SIZE:
-                    translated_chunks.append(ChunkToWordsCounters(translated_chunk, corpusNouns))
+                    translated_chunks.append(ChunkToWordsCounters(translated_chunk, corpusNouns, search_enum))
                     translated_chunk = []
-    original_chunks.append(ChunkToWordsCounters(original_chunk, corpusNouns))
-    translated_chunks.append(ChunkToWordsCounters(translated_chunk, corpusNouns))
+    original_chunks.append(ChunkToWordsCounters(original_chunk, corpusNouns, search_enum))
+    translated_chunks.append(ChunkToWordsCounters(translated_chunk, corpusNouns, search_enum))
     return original_chunks, translated_chunks
 
 
@@ -87,38 +111,68 @@ def incrementCount(map, key):
 
 def RunOneFold(dataTrain, dataTest, labelsTrain, labelsTest, queue):
     print("alive!")
-    # clf = svm.SVC(kernel='rbf').fit(X_train, y_train)
-    # predicted = clf.predict(X_test)
-    clfModel = svmutil.svm_train(labelsTrain, dataTrain, '-s 0 -t 2 -q -c 1')
-    predicted = svmutil.svm_predict(labelsTest, dataTest, clfModel, '-q')
-    predicted = np.array(predicted[0])
-    score = np.sum(predicted == y_test) / len(y_test)
-    print(score)
+    clf = svm.SVC(kernel='rbf', gamma=0.001, C=0.01).fit(dataTrain, labelsTrain)
+    predicted = clf.predict(dataTest)
+    #clfModel = svmutil.svm_train(labelsTrain, dataTrain, '-s 0 -t 2 -q -c 1')
+    #predicted = svmutil.svm_predict(labelsTest, dataTest, clfModel, '-q')
+    #predicted = np.array(predicted[0])
+    predicted = np.array(predicted)
+    score = np.sum(predicted == labelsTest) / len(labelsTest)
+    print("printing one process dist: " + current_process().name)
+    print(current_process().name + "----predicted as original: " + str(np.sum(np.array(predicted) == 1)) + " predicted as translated:" + str(np.sum(np.array(predicted) == 0)))
+    print(current_process().name + "----labeled as original: " + str(np.sum(np.array(labelsTest) == 1)) + " labeled as translated:" + str(np.sum(np.array(labelsTest) == 0)))
+    print(current_process().name + " process score: " + str(score))
     queue.put_nowait(score)
 
 if __name__ == '__main__':
     print( "***Starting Time*** "  + datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-    pronouns = readWordsFromFile('D:\\GitHub\\NLP_Lab\\pronouns.txt')
-    function_words = readWordsFromFile('D:\\GitHub\\NLP_Lab\\function_words.txt')
+    parser = argparse.ArgumentParser(description='This script runs an ML trained model according to specified features')
+    parser.add_argument('features', help='Search for words or post in corpus POSBigrams POSTrigrams FunctionWords Pronouns',
+                        choices=['POSBigrams', 'POSTrigrams', 'FunctionWords', 'Pronouns'])
+    parser.add_argument('--pronouns_path', help='Specify the pronouns file path', required=True)
+    parser.add_argument('--funcwords_path', help='Specify the function words file path', required=True)
+    parser.add_argument('--english_path', help='Specify the english sentences file(tagged,translated&non-translated)', required=True)
+    parser.add_argument('--labels_path', help='Specify the english labels file(translated&non-translated)', required=True)
+    args = parser.parse_args()
+    print(args)
+    #english_file_path = "/home/ohadmosafi/NLP_Lab/en_m_tagged.txt"
+    #label_file_path = "/home/ohadmosafi/NLP_Lab/fr-en_m.txt"
 
-    english_file_path = "D:\\GitHub\\NLP_Lab\\local_results_afterFix_link_dict\\Results2017-05-23_04-09-16\\openNLP\\en_tagged.txt"
-    label_file_path = "D:\\GitHub\\NLP_Lab\\local_results_afterFix_link_dict\\Results2017-05-23_04-09-16\\fr-en.txt"
-    #english_file_path = "D:\\GitHub\\NLP_Lab\\en_m_tagged.txt"
-    #label_file_path = "D:\\GitHub\\NLP_Lab\\fr-en_m.txt"
+    if args.features == "Pronouns" or args.features == "FunctionWords":
+        if args.features == "Pronouns":
+            file_words = readWordsFromFile(args.pronouns_path)
+        elif args.features == "FunctionWords":
+            file_words = readWordsFromFile(args.funcwords_path)
+        valid_words_in_corpus = wordsInCorpus(args.english_path, file_words)
+        o, t = divide_to_chnuks(args.english_path, args.labels_path, 'en', valid_words_in_corpus, SearchWordsOrPOS.FIND_WORDS)
 
-    valid_words_in_corpus = wordsInCorpus(english_file_path, function_words)
-    #valid_words_in_corpus = POSinCorpus(english_file_path,3)
-    o, t = divide_to_chnuks(english_file_path, label_file_path, 'en', valid_words_in_corpus)
+    if "POS" in args.features:
+        if args.features == "POSTrigrams":
+            valid_words_in_corpus = POSinCorpus(args.english_path, 3)
+        elif args.features == "POSBigrams":
+            valid_words_in_corpus = POSinCorpus(args.english_path, 2)
+        o, t = divide_to_chnuks(args.english_path, args.labels_path, 'en', valid_words_in_corpus, SearchWordsOrPOS.FIND_POS)
+
+    np.random.shuffle(o)
+    np.random.shuffle(t)
 
     data = []
     label = []
-    for chunk in o:
-        data.append(chunk)
+    print("no. of sampels in original: " + str(len(o)))
+    print("no. of sampels in translated: " + str(len(t)))
+    for i in range(len(t)):
+        data.append(o[i])
         label.append(1)
-    for chunk in t:
-        data.append(chunk)
+    for i in range(len(t)):
+        data.append(t[i])
         label.append(0)
+
     print("***Starting MultiProcesses*** " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+    print("number of samples:" + str(len(label)))
+    print("number of original lang samples: " + str(np.sum(np.array(label) == 1)))
+    print("number of translated lang samples: " + str(np.sum(np.array(label) == 0)))
+    print("number of features:" + str(len(data[0])) + " or " + str(len(valid_words_in_corpus)) )
+
     folds = 10
     kf = KFold(n_splits=folds, shuffle=True)
     q = Queue()
@@ -135,15 +189,6 @@ if __name__ == '__main__':
         p.join()
     countFolds = 0
     while (not q.empty()):
-        countFolds +=  q.get()
+        countFolds += q.get()
     print ("total success rate is:" + str(countFolds/folds))
     print("***Finish Time*** " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-
-
-    #clf = svm.SVC(kernel='linear') # .fit(data, label)
-    #print(clf.score(data, label))
-    #scores = cross_val_score(clf, data, label, cv=2)
-    #print(scores)
-
-    # pronounsCount = {}
-    # incrementCount(pronounsCount, token)
